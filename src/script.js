f0027ce7ea362a52876d72f507cd14a0b5960e9f
Ioannis Antoniou <johnantony24@hotmail.com>
2023-03-23 20:50:02 +0100

    Handling Fullscreen and Resizing

--- src/script.js
+++ src/script.js
@@ -1,4 +1,19 @@
 import * as THREE from 'three'
+import './style.css'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x:0,
+  y:0
+}
+window.addEventListener('mousemove',(event)=>
+{
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = -(event.clientY / sizes.height - 0.5)
+})
 
 /**
  * Base
@@ -8,9 +23,53 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+  width: window.innerWidth,
+  height: window.innerHeight
 }
+window.addEventListener('resize', () =>
+{
+  //Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  //Update Camera
+  camera.aspect = sizes.width /sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+})
+
+window.addEventListener('dblclick', () =>
+{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullscreenElement)
+    {
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else
+    {
+        if(document.exitFullscreen)
+        {
+            document.exitFullscreen()
+        }
+        else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+})
 
 // Scene
 const scene = new THREE.Scene()
@@ -20,6 +79,19 @@ const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
+// Create an empty BufferGeometry
+const geometry = new THREE.BufferGeometry()
+
+// Create a Float32Array containing the vertices position (3 by 3)
+const positionsArray = new Float32Array([
+    0, 0, 0, // First vertex
+    0, 1, 0, // Second vertex
+    1, 0, 0  // Third vertex
+])
+
+// Create the attribute and name it 'position'
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
 scene.add(mesh)
 
 // Camera
@@ -29,6 +101,11 @@ camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+//Controls
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -42,9 +119,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    //Update Controls
+    controls.update()
+    //  Update objects
+    // mesh.rotation.y = elapsedTime;
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    // Update Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI*2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI*2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
