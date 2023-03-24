a8372bf192753f5da4988fb2b40aafd149fff71f
Ioannis Antoniou <johnantony24@hotmail.com>
2023-03-24 10:48:49 +0100

    Textures

--- src/script.js
+++ src/script.js
@@ -1,19 +1,5 @@
 import * as THREE from 'three'
-import './style.css'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-
-/**
- * Cursor
- */
-const cursor = {
-  x:0,
-  y:0
-}
-window.addEventListener('mousemove',(event)=>
-{
-  cursor.x = event.clientX / sizes.width - 0.5
-  cursor.y = -(event.clientY / sizes.height - 0.5)
-})
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
@@ -21,114 +7,74 @@ window.addEventListener('mousemove',(event)=>
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
-}
-window.addEventListener('resize', () =>
-{
-  //Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
-
-  //Update Camera
-  camera.aspect = sizes.width /sizes.height
-  camera.updateProjectionMatrix()
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-
-})
-
-window.addEventListener('dblclick', () =>
-{
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-    if(!fullscreenElement)
-    {
-        if(canvas.requestFullscreen)
-        {
-            canvas.requestFullscreen()
-        }
-        else if(canvas.webkitRequestFullscreen)
-        {
-            canvas.webkitRequestFullscreen()
-        }
-    }
-    else
-    {
-        if(document.exitFullscreen)
-        {
-            document.exitFullscreen()
-        }
-        else if(document.webkitExitFullscreen)
-        {
-            document.webkitExitFullscreen()
-        }
-    }
-})
-
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-// Create an empty BufferGeometry
-const geometry = new THREE.BufferGeometry()
-
-// Create a Float32Array containing the vertices position (3 by 3)
-const positionsArray = new Float32Array([
-    0, 0, 0, // First vertex
-    0, 1, 0, // Second vertex
-    1, 0, 0  // Third vertex
-])
-
-// Create the attribute and name it 'position'
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
-geometry.setAttribute('position', positionsAttribute)
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
-//Controls
-
+// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-    //Update Controls
-    controls.update()
-    //  Update objects
-    // mesh.rotation.y = elapsedTime;
 
-    // Update Camera
-    // camera.position.x = Math.sin(cursor.x * Math.PI*2) * 3
-    // camera.position.z = Math.cos(cursor.x * Math.PI*2) * 3
-    // camera.position.y = cursor.y * 5
-    // camera.lookAt(mesh.position)
+    // Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
